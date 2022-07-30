14368595e52fcf00b186c9430bec117d6c35c2d5
Juan Martinez <jemartinezpucheta@gmail.com>
2022-07-30 17:50:26 -0300

    first commit

---
+++ api/src/Controllers/DogsController.js
@@ -0,0 +1,3 @@
+const axios = require('axios');
+const { Dog, Temperament } = require ('../db.js');
+const { API_KEY } = process.env
---
+++ api/src/routes/Dogs.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+
+const router = Router();
+
+router.get ( '/get', )
+router.get ( '/get', )
+
+router.post ( '/create', )
+
+module.exports = router;
